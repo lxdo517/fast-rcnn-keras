f89ed666ad20bc9fda871e4d0ce511d1f35c319b
xiaoxu1025 <1312759081@qq.com>
2020-01-28 17:40:48 +0800

    update

--- models/vgg16_body.py
+++ models/vgg16_body.py
@@ -1,14 +1,19 @@
-from tensorflow.keras import Model
 from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras import Model
+from tensorflow.keras.layers import Input
 
 
 def get_model_body(trainable=False):
-    vgg16_model = VGG16(include_top=False)
+    input_tensor = Input(shape=(567, 567, 3))
+    vgg16_model = VGG16(input_tensor=input_tensor, include_top=False)
     if not trainable:
         for layer in vgg16_model.layers:
             # 让其不可训练
             layer.trainable = False
-    vgg16_model.layers.pop()
-    model = Model(inputs=vgg16_model.input, outputs=vgg16_model.output)
+    model = Model(inputs=vgg16_model.input, outputs=vgg16_model.get_layer('block5_conv3').output)
+    # model = Model(inputs=vgg16_model.input, outputs=vgg16_model.output)
     return model
+
+#
+# model = get_model_body()
+# model.summary()
